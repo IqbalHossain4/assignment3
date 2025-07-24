394630a71536916f4894ac914fd3ca5a9134b1bf
IqbalHossain4 <iqbal-hossain@mail.com>
2025-07-24 20:15:25 +0600

    borrowed data updated

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,9 @@ const express_1 = __importDefault(require("express"));
 const book_controller_1 = require("./app/controllers/book.controller");
 const borrow_controller_1 = require("./app/controllers/borrow.controller");
 const app = (0, express_1.default)();
+var cors = require("cors");
 app.use(express_1.default.json());
+app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 app.use("/api/books", book_controller_1.bookRoutes);
 app.use("/api/borrow", borrow_controller_1.borrowRoutes);
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import { bookRoutes } from "./app/controllers/book.controller";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
 
 const app: Application = express();
+var cors = require("cors");
 app.use(express.json());
+app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 app.use("/api/books", bookRoutes);
 app.use("/api/borrow", borrowRoutes);
 
